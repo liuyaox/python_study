be2da41d06e6aa9ef90dd5ea301c5916ee1d7758
liuyaox <siberiawolf61@hotmail.com>
2019-05-31 18:22:54 +0800

    add detailed comment

--- argparse_demo.py
+++ argparse_demo.py
@@ -2,39 +2,43 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Jan 29 00:03:58 2019
-
 @author: liuyao8
 """
 
 import argparse
 
 parser = argparse.ArgumentParser(description='Argparse Test.')
-parser.add_argument(        # Positional Arguments
+parser.add_argument(        # Positional Arguments   用【参数先后位置】来区分各参数
         'integers',
         metavar='N',
         type=int,
-        # Number of arguments  
-        # N: N arguments present will be gathered into a list  
-        # ?: one argument if possible and produced as a single item. If no argument present, use default
-        # *: All arguments present will be gathered into a list
-        # +: Just like '*'
-        # argparse.REMAINDER: All the remaining arguments will be gathered into a list.
+        
+        # nargs: Number of arguments  
+        #   N: N arguments present will be gathered into a list  
+        #   ?: one argument if possible and produced as a single item. If no argument present, use default
+        #   *: All arguments present will be gathered into a list
+        #   +: 同 '*'
+        #   argparse.REMAINDER: All the remaining arguments will be gathered into a list.
         nargs='+',
         
         help='An integer for the accumulator'
 )
-parser.add_argument(        # Optional Arguments
+parser.add_argument(        # Optional Arguments   用【前缀-或--】来区分各参数
         '--sum',
+        
         # dest: The name of the attribute to be added to the object returned by parse_args()
         dest='accumulate',
+        
         # action: ArgumentParser objects associate arguments with actions
-        # store: default, just store argument's value
-        # store_const: store argument's value specified by the const keyword argument
+        #   store: default, just store argument's value
+        #   store_const: store argument's value specified by the const keyword argument
         action='store_const',
+        
         # const: Used to hold constant values that are not read from command line but are required for the various ArgumentParser actions
-        # When action='store_const' or 'append_const'
-        # When called with option strings (like -f or --foo) and nargs='?'
+        #   When action='store_const' or 'append_const'
+        #   When called with option strings (like -f or --foo) and nargs='?'
         const=sum,
+        
         default=max,
         help='sum the integers (default: find the max)'        
 )
